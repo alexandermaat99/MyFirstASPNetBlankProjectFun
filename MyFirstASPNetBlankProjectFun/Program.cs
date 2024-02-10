036d187795c7b7f7059fe6c5faf714f53ed5bff1
Alexander Maat <amaat@byu.edu>
2024-02-09 21:27:33 -0700

    done with videos

--- MyFirstASPNetBlankProjectFun/Program.cs
+++ MyFirstASPNetBlankProjectFun/Program.cs
@@ -1,7 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.UseDefaultFiles();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
